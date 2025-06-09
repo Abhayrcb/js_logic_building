02b3645bde927a05f7b65dfd8443c1a01aec6c8a
Abhayrcb <abhaychoudhary4578@gmail.com>
2025-06-10 00:07:30 +0530

    third problem

---
+++ problem_no3.js
@@ -0,0 +1,10 @@
+function calculateCircleArea(radius) {
+    const pi = Math.PI;
+    let area = pi*radius*radius;
+
+    return radius,area;
+}
+
+let radius,area = calculateCircleArea(5)
+
+console.log(`radius=${radius},area=${area}`); // Expected output: 78.53981633974483
